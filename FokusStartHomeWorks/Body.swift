487fba0425b950a016cd9bebe0241b87d172f586
Anton Senokosov <asenokosov84@gmail.com>
2020-10-24 18:24:17 +0700

    реализовал автокаталог

---
+++ FokusStartHomeWorks/Body.swift
@@ -0,0 +1,14 @@
+//  CarBody.swift
+//  FokusStartHomeWorks
+//
+//  Created by Я on 16/10/2020.
+//  Copyright © 2020 HomeMade. All rights reserved.
+//
+
+import Foundation
+
+enum Body {
+    case Unknown
+    case Hatchback
+    case Sedan
+}
---
+++ FokusStartHomeWorks/Car.swift
@@ -0,0 +1,16 @@
+//  Car.swift
+//  FokusStartHomeWorks
+//
+//  Created by Я on 16/10/2020.
+//  Copyright © 2020 HomeMade. All rights reserved.
+//
+
+import Foundation
+
+struct Car {
+	var manufacturer: String = ""
+    var model: String = ""
+    var body = Body.Unknown
+    var yearOfIssue: String = "-"
+    var carNumber: String = ""
+}
