71fccd3364872710bb3bf463bb4ffcad91c54318
Anton Senokosov <asenokosov84@gmail.com>
2020-10-27 17:56:47 +0700

    Реализовано: создал и вынес в отдельный файл класс "Потокобезопасны…
    …й массив".
    
    Для него написал расширения с методами: -append(_ item:)
    							               - remove(at index: Int)
    								       - subscript(index: Int)
    								       - contains(_ element:)
    и свойствами: - IsEmpty и count.

--- ThreadSafeArray/ThreadSafeArray/main.swift
+++ ThreadSafeArray/ThreadSafeArray/main.swift
@@ -2,8 +2,36 @@
 //  main.swift
 //  ThreadSafeArray
 //
-//  Created by Uzver on 24.10.2020.
+//  Created by Uzver on 27.10.2020.
 //
 
 import Foundation
 
+var array1 = ThreadSafeArray<Any>()
+var array2 = [Int]()
+
+let group = DispatchGroup()
+
+group.enter()
+
+DispatchQueue.global(qos: .userInteractive).async {
+	for i in 1...1000 {
+		array1.appendTSA(i)
+		print("Выполняю потокобезопасный")
+	}
+	group.leave()
+}
+
+group.enter()
+
+DispatchQueue.global(qos: .userInteractive).async {
+	for i in 1...1000 {
+		array2.append(i)
+		print("Выполняю обычный массив")
+	}
+	group.leave()
+}
+group.wait()
+
+print(array1.count)
+print(array2.count)
