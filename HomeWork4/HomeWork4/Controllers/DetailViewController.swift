2a8c5dc7f8d6eb6bccc485ba19d6e876fbdc3651
Anton Senokosov <asenokosov84@gmail.com>
2020-11-09 12:56:24 +0700

    в соответствии с требованиями реализовал закругление на изображениях и подложил под них тень. реализовал через отдельное расширение для UIView.

--- HomeWork4/HomeWork4/Controllers/DetailViewController.swift
+++ HomeWork4/HomeWork4/Controllers/DetailViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 class DetailViewController: UIViewController {
 
 	@IBOutlet weak var labelDetailView: UILabel!
+
 	@IBOutlet weak var imageOneDetailView: UIImageView!
 	@IBOutlet weak var imageTwoDetailView: UIImageView!
+
+	@IBOutlet weak var viewImageOne: UIView!
+	@IBOutlet weak var viewImageTwo: UIView!
 
 	var cellObject: CellsModel? {
 		didSet {
@@ -21,11 +25,20 @@ class DetailViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		}
-	
+		setShadow()
+
+	}
+
+	func setShadow() {
+		viewImageOne.shadowImage(cornerRadius: 10, shadowColor: .black, shadowRadius: 10)
+		imageOneDetailView.shadowImage(cornerRadius: 10, shadowColor: .black, shadowRadius: 10)
+		viewImageTwo.shadowImage(cornerRadius: 10, shadowColor: .black, shadowRadius: 10
+		)
+		imageTwoDetailView.shadowImage(cornerRadius: 10, shadowColor: .black, shadowRadius: 10)
+	}
+
 	func set() {
 		loadViewIfNeeded()
-		imageShadowSettings()
 		imageOneDetailView.image = cellObject?.imageFirst
 		imageTwoDetailView.image = cellObject?.secondImage
 		labelDetailView.text = cellObject?.smallDescription
@@ -33,15 +46,19 @@ class DetailViewController: UIViewController {
 	}
 }
 
-
 extension DetailViewController: NewsObjectSelectedDelegate {
 	func newsObjectSelected(newsObject: CellsModel) {
 		self.cellObject = newsObject
 	}
 }
-
-extension DetailViewController {
-	func imageShadowSettings() {
-		imageOneDetailView.layer.cornerRadius = 8
+extension UIView {
+	func shadowImage(cornerRadius: CGFloat, shadowColor: UIColor, shadowOffset: CGSize = .zero, shadowOpacity: Float = 1, shadowRadius: CGFloat) {
+		layer.cornerRadius = cornerRadius
+		layer.shadowColor = shadowColor.cgColor
+		layer.shadowOffset = shadowOffset
+		layer.shadowOpacity = shadowOpacity
+		layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+		layer.shadowRadius = shadowRadius
 	}
 }
+
